3d83a94740e51f2ad46c81cdb714de4a4ae26377
bintariq29 <hanzlat478@gmail.com>
2026-01-20 14:53:56 +0500

    folder structure complete by command prompt

---
+++ src/app/heros/create-hero/create-hero-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-hero-dialog',
+  imports: [],
+  templateUrl: './create-hero-dialog.component.html',
+  styles: ``
+})
+export class CreateHeroDialogComponent {
+
+}
---
+++ src/app/heros/edit-hero/eidt-hero-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eidt-hero-dialog',
+  imports: [],
+  templateUrl: './eidt-hero-dialog.component.html',
+  styles: ``
+})
+export class EidtHeroDialogComponent {
+
+}
