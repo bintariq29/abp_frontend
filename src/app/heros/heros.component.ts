c65a98db640ce72c063834563753f9ca25165106
bintariq29 <hanzlat478@gmail.com>
2026-01-21 19:26:05 +0500

    ..

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,67 +1,52 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@node_modules/@angular/forms';
+import { ChangeDetectorRef, Component, Injector } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LazyLoadEvent, PrimeTemplate } from 'primeng/api';
+import { PaginatorModule } from 'primeng/paginator';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { PagedListingComponentBase } from '@shared/paged-listing-component-base';
 import { LocalizePipe } from '@shared/pipes/localize.pipe';
-import { HeroAppServicServiceProxy } from '@shared/service-proxies/service-proxies';
+import { HeroAppServicServiceProxy, HeroDto } from '@shared/service-proxies/service-proxies';
 
 
 @Component({
-  imports: [LocalizePipe , FormsModule],
+  imports: [LocalizePipe, FormsModule, PaginatorModule, PrimeTemplate],
   templateUrl: './heros.component.html',
   standalone: true,
   providers: [HeroAppServicServiceProxy],
   animations: [appModuleAnimation()]
 })
-export class HerosComponent {
-  keyword='';
-  advancedFiltersVisible=true;
-  constructor(
-    private _heroService: HeroAppServicServiceProxy
-  ) {
-
+export class HerosComponent extends PagedListingComponentBase<HeroDto> {
+  protected delete(entity: HeroDto): void {
+    throw new Error('Method not implemented.');
   }
+  keyword = '';
+  advancedFiltersVisible = false;
+
+  constructor(
+
+    private _heroService: HeroAppServicServiceProxy,
+    injector: Injector,
+    cd: ChangeDetectorRef
+  ) {
+    super(injector, cd);
+  }
+
+
 
   createUser() {
     this._heroService
   }
 
-  list(): void {
+  list(event?: LazyLoadEvent): void {
     alert(this.keyword)
 
 
   }
 
+  refresh(): void {
 
-  // list(event?: LazyLoadEvent): void {
-  //     if (this.primengTableHelper.shouldResetPaging(event)) {
-  //         this.paginator.changePage(0);
+  }
 
-  //         if (this.primengTableHelper.records && this.primengTableHelper.records.length > 0) {
-  //             return;
-  //         }
-  //     }
 
-  //     this.primengTableHelper.showLoadingIndicator();
-
-  //     this._userService
-  //         .getAll(
-  //             this.keyword,
-  //             this.isActive,
-  //             this.primengTableHelper.getSorting(this.dataTable),
-  //             this.primengTableHelper.getSkipCount(this.paginator, event),
-  //             this.primengTableHelper.getMaxResultCount(this.paginator, event)
-  //         )
-  //         .pipe(
-  //             finalize(() => {
-  //                 this.primengTableHelper.hideLoadingIndicator();
-  //             })
-  //         )
-  //         .subscribe((result: UserDtoPagedResultDto) => {
-  //             this.primengTableHelper.records = result.items;
-  //             this.primengTableHelper.totalRecordsCount = result.totalCount;
-  //             this.primengTableHelper.hideLoadingIndicator();
-  //             this.cd.detectChanges();
-  //         });
-  // }
 
 }
--- src/app/heros/heros.module.ts
+++ src/app/heros/heros.module.ts
@@ -4,11 +4,13 @@ import { HerosRoutingModule } from './heros-routing.module';
 import { CreateHeroDialogComponent } from './create-hero/create-hero-dialog.component';
 import { EidtHeroDialogComponent } from './edit-hero/eidt-hero-dialog.component';
 import { HerosComponent } from './heros.component';
+import { SharedModule } from '@node_modules/primeng/api';
 
 
 @NgModule({
   declarations: [],
   imports: [
+    SharedModule,
     CommonModule,
     HerosRoutingModule,
     CreateHeroDialogComponent,
