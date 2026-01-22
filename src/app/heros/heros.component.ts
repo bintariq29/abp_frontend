00e5aa9e230cb8d3e99167ab043dcaba9c06a1ad
bintariq29 <hanzlat478@gmail.com>
2026-01-22 14:57:53 +0500

    data populating

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,51 +1,60 @@
-import { ChangeDetectorRef, Component, Injector } from '@angular/core';
+import { ChangeDetectorRef, Component, Injector, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { LazyLoadEvent, PrimeTemplate } from 'primeng/api';
+import { ConfirmationService, LazyLoadEvent, PrimeTemplate } from 'primeng/api';
 import { PaginatorModule } from 'primeng/paginator';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
-import { PagedListingComponentBase } from '@shared/paged-listing-component-base';
+
 import { LocalizePipe } from '@shared/pipes/localize.pipe';
 import { HeroAppServicServiceProxy, HeroDto } from '@shared/service-proxies/service-proxies';
+import { AppComponentBase } from '@shared/app-component-base';
+import { TableModule } from 'primeng/table';
 
 
 @Component({
-  imports: [LocalizePipe, FormsModule, PaginatorModule, PrimeTemplate],
+  imports: [LocalizePipe, FormsModule, PaginatorModule, TableModule,
+  ],
   templateUrl: './heros.component.html',
   standalone: true,
-  providers: [HeroAppServicServiceProxy],
+  providers: [HeroAppServicServiceProxy, ConfirmationService],
   animations: [appModuleAnimation()]
 })
-export class HerosComponent extends PagedListingComponentBase<HeroDto> {
+export class HerosComponent extends AppComponentBase implements OnInit {
   protected delete(entity: HeroDto): void {
     throw new Error('Method not implemented.');
   }
   keyword = '';
   advancedFiltersVisible = false;
+  heros: any[] = [];
 
   constructor(
 
     private _heroService: HeroAppServicServiceProxy,
+    private _cd: ChangeDetectorRef,
     injector: Injector,
-    cd: ChangeDetectorRef
+    cd: ChangeDetectorRef,
+    private confirmationService: ConfirmationService
   ) {
-    super(injector, cd);
+    super(injector);
+  }
+  ngOnInit(): void {
+
+    this.getHeros();
+  }
+
+  getHeros() {
+    this._heroService.getAllHeros().subscribe((result) => {
+      this.heros = result.items;
+      this._cd.detectChanges();
+    })
+  }
+
+  createHero() {
+
   }
 
 
 
-  createUser() {
-    this._heroService
-  }
 
-  list(event?: LazyLoadEvent): void {
-    alert(this.keyword)
-
-
-  }
-
-  refresh(): void {
-
-  }
 
 
 
--- src/app/heros/heros.module.ts
+++ src/app/heros/heros.module.ts
@@ -4,7 +4,7 @@ import { HerosRoutingModule } from './heros-routing.module';
 import { CreateHeroDialogComponent } from './create-hero/create-hero-dialog.component';
 import { EidtHeroDialogComponent } from './edit-hero/eidt-hero-dialog.component';
 import { HerosComponent } from './heros.component';
-import { SharedModule } from '@node_modules/primeng/api';
+import { SharedModule } from 'primeng/api';
 
 
 @NgModule({
@@ -15,8 +15,7 @@ import { SharedModule } from '@node_modules/primeng/api';
     HerosRoutingModule,
     CreateHeroDialogComponent,
     EidtHeroDialogComponent,
-    HerosComponent
-
+    HerosComponent,
   ]
 })
 export class HerosModule { }
